4f9fb802bde51bd9708cd91a7aa17f7ad0f765f2
billihazero <billihazero@gmail.com>
2024-08-19 18:28:25 +0900

    first commit

---
+++ src/main/java/com/example/ramenddang/RamenddangApplication.java
@@ -0,0 +1,13 @@
+package com.example.ramenddang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RamenddangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RamenddangApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ramenddang/RamenddangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ramenddang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RamenddangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
